58981ffeb14508eadb237e8d213f037f4b398285
Developer <dev@vibeshare.local>
2026-05-28 09:51:13 +0800

    fix: restrict image domains, move prisma to devDeps, remove unused deps

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
+        hostname: "*.r2.cloudflarestorage.com",
       },
     ],
   },
